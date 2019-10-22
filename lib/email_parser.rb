d0ce3a1d3e0ffffef14de5d0d65e0e434f0fda60
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-22 15:33:23 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,9 +1,17 @@
-def EmailAddressParser
-  attr_accessor :emails
-
+  class EmailParser
+    
+   @@all = []
+  attr_accessor :email_addresses
+    def initialize(email_addresses)
+      @@parser = parser
+      @emails = email_addresses
+      
   def self.parse
+    @email_addresses.map! { |e| e.split (",", " ") }
+    @@all << @email_addresses
+    @@all.uniq!
+    end
   end
-
 end
   # Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
